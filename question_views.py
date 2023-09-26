0bbc8e67e5c0ae7621c5a03ef93713af3d9fb3dc
은지 <dmswl6842@naver.com>
2023-09-26 17:29:49 +0900

    점프투플라스크따라하다가,,

---
+++ question_views.py
@@ -0,0 +1,9 @@
+from pybo.forms import QuestionForm
+from flask import Blueprint, render_template, url_for
+
+bp = Blueprint('main', __name__, url_prefix='/')
+
+@bp.route('/create/')
+def create():
+    form = QuestionForm()
+    return render_template('question/question_form.html', form=form)
